d504cdcbacbe4dd57429e45f7cc51aa5617985f5
Pragnesh-0 <pragneshuno@gmail.com>
2026-02-24 12:39:50 +0400

    Beatmaps

---
+++ Assets/UtilityScripts/BeatmapModels.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class BeatmapModels
+{
+    public int score;
+    public List<BeatData> beats;
+}
+
+[System.Serializable]
+public class BeatData
+{
+    public float time;
+    public string type;
+}
